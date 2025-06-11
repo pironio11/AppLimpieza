b599214bf01eb538dfed4d8d1b2d44984ef3652b
pironio11 <joapironio@gmail.com>
2025-06-10 22:26:43 -0300

    testeo y formato de FRMinicio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './FRM.css';
 
 function App() {
   return (
--- src/componentes/FRMRegistre.jsx
+++ src/componentes/FRMRegistre.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import frmstyle from "../FRM.css";
 //aca se importan los datos que necesita el fotmulario
 
 const FRMregistro =() => {
 
     return(
-         <div className="a" >
-            <button onClick={() => alert('!hiciste click!')}>
-            google
-            </button>
-         </div>
+            <div className="container-FRM">
+                <h2>Bienvenido a la app de limpieza</h2>
+                <div className="botongoogle" >
+                     <button onClick={() => alert('!hiciste click!')}>
+                     google
+                     </button>
+                </div>
+                <div className="enviar">
+                    <button type="submit">Enviar</button>
+                </div>
+            </div>
     )
 }
 
