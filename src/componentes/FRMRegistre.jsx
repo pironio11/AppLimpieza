8f68d8c83553e3d985a8d1a1cc30ef148972974d
pironio11 <joapironio@gmail.com>
2025-06-10 21:04:03 -0300

    arregloFRM registro del boton

--- src/componentes/FRMRegistre.jsx
+++ src/componentes/FRMRegistre.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import "../css/FRMregistro.css";
 //aca se importan los datos que necesita el fotmulario
 
-const FRMregistro = ({ titulo = "formulario inicio de sesion" }) => {
+const FRMregistro =() => {
 
     return(
-         <div>
+         <div className="a" >
             <button onClick={() => alert('!hiciste click!')}>
             google
             </button>
          </div>
     )
 }
+
+export default FRMregistro;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import FRMRegistre from './componentes/FRMRegistre';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <FRMRegistre />
   </React.StrictMode>
 );
 
