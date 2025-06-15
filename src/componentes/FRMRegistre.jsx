a27958e1dd0c5934e13613c20b1d7c8ea3911ec8
pironio11 <joapironio@gmail.com>
2025-06-15 15:23:06 -0300

    creacion de foto de epet20 y estilos

--- src/componentes/FRMRegistre.jsx
+++ src/componentes/FRMRegistre.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import frmstyle from "../FRM.css";
+import epetfoto from '../imagenes/epetfoto.jpg';
 //aca se importan los datos que necesita el fotmulario
 
 const FRMregistro =() => {
 
     return(
             <div className="container-FRM">
-                <div classname="titulo1"><h1>Limpieza continua</h1></div>
-                <div classname="titulo2"><h2>Bienvenido a la app de limpieza</h2></div>
+                <div className="fotoEpet"><img src={epetfoto} alt="imagen de epet20" heigth="168" width="300" title="epet20"></img></div>
+                <div className="titulo1"><h1>Limpieza continua</h1></div>
+                <div className="titulo2"><h2>Bienvenido a la app de limpieza</h2></div>
                 <div className="botongoogle" >
                      <button onClick={() => alert('!hiciste click!')}>
                      google
