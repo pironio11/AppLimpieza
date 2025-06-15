297f3ceb1e5b4d183db0dec066cced8e59183ae2
pironio11 <joapironio@gmail.com>
2025-06-15 15:58:22 -0300

    foto de google boton inicio de sesion

--- src/componentes/FRMRegistre.jsx
+++ src/componentes/FRMRegistre.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import frmstyle from "../FRM.css";
 import epetfoto from '../imagenes/epetfoto.jpg';
+import googlefoto from '../imagenes/googleFoto.jpg';
 //aca se importan los datos que necesita el fotmulario
 
 const FRMregistro =() => {
@@ -10,14 +11,12 @@ const FRMregistro =() => {
                 <div className="fotoEpet"><img src={epetfoto} alt="imagen de epet20" heigth="168" width="300" title="epet20"></img></div>
                 <div className="titulo1"><h1>Limpieza continua</h1></div>
                 <div className="titulo2"><h2>Bienvenido a la app de limpieza</h2></div>
-                <div className="botongoogle" >
-                     <button onClick={() => alert('!hiciste click!')}>
-                     google
-                     </button>
-                </div>
-                <div className="enviar">
-                    <button type="submit">Enviar</button>
-                </div>
+                    <div className="botongoogle" >
+                        <button onClick={() => alert('!FireBase en proceso!')}>
+                            <img src={googlefoto} height={20} width={20}></img>
+                            inicia sesion con google
+                        </button>
+                    </div>
             </div>
     )
 }
