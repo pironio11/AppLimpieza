7bfa9aea3b140f2629e2884864ec1539abafadce
pironio11 <joapironio@gmail.com>
2025-06-15 21:06:46 -0300

    creacion de rama para hacer pruebas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
+
 import './FRM.css';
-=======
+
 import EncargadoLimpieza from './componentes/encargado_limpieza';
 import Navbar from './componentes/navbar';
 import Footer from './componentes/footer';
->>>>>>> encargado_limpieza
+
 
 function App() {
   return (
