c145fb48ab365ff345f39af3e393feecd37e9689
pironio11 <joapironio@gmail.com>
2025-06-15 21:58:50 -0300

    footer a paginas principales

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import FRMRegistre from './componentes/FRMRegistre';
+import Footer from './componentes/footer';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <FRMRegistre />
+    <Footer/>
   </React.StrictMode>
 );
 
