709abd32f2bc132bfdc14e97f106444fa4d6c077
alejo <alejovillarroelsape@gmail.com>
2025-06-16 17:39:49 -0300

    modi

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-<<<<<<< HEAD
-import './FRM.css';
-=======
-import EncargadoLimpieza from './componentes/encargado_limpieza';
-import Navbar from './componentes/navbar';
-import Footer from './componentes/footer';
->>>>>>> encargado_limpieza
 
+import './App.css';
+import EncargadoLimpieza from './componentes/encargado_limpieza';
+import Footer from './componentes/footer';
+import Navbar from './componentes/navbar';
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <EncargadoLimpieza />
-      <Footer />
+        <EncargadoLimpieza/>
+        <Footer/>
+        <Navbar/>
+
+        <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
