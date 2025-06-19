b6f00075a7a4999423849bed23ded7c6726bb062
pironio11 <joapironio@gmail.com>
2025-06-19 20:20:28 -0300

    Acomode lo que sobra de FRM

--- src/componentes/FRMRegistre.jsx
+++ src/componentes/FRMRegistre.jsx
@@ -8,17 +8,7 @@ const FRMregistro =() => {
 
     return(
             <div className="container-FRM">
-                <div className="foto"><img src="../fotos/descarga.jfif"></img></div>
-                <div classname="titulo1"><h1>Limpieza continua</h1></div>
-                <div classname="titulo2"><h2>Bienvenido a la app de limpieza</h2></div>
-                <div className="botongoogle" >
-                     <button onClick={() => alert('!hiciste click!')}>
-                     google
-                     </button>
-                </div>
-                <div className="enviar">
-                    <button type="submit">Enviar</button>
-                </div>
+                
 
                 <div className="fotoEpet"><img src={epetfoto} alt="imagen de epet20" heigth="168" width="300" title="epet20"></img></div>
                 <div className="titulo1"><h1>Limpieza continua</h1></div>
