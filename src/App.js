842767174d9002ec290098d6009f8a2b10d61058
FlAuStO <borquez.faustino.epet20@gmail.com>
2025-09-03 23:23:39 -0300

    fix: corregir error de importacion duplicada en App.js y restaurar router funcional

--- src/App.js
+++ src/App.js
@@ -5,34 +5,21 @@ import VistaAdmin from './views/admin/VistaAdmin';
 import VistaUsuario from './views/usuario/VistaUsuario';
 import './App.css';
 import EncargadoLimpieza from './componentes/encargado_limpieza';
-import FRMregistro from './componentes/FRMRegistre';
 import ReportesProblemas from './componentes/ReportesProblemas';
 import Configuracion from './componentes/Configuracion';
 import Tareas from './componentes/tareas';
 
 function App() {
   return (
-    <div className="App">
-        <FRMregistro/>
-        {/* <EncargadoLimpieza/>
-        <ReportesProblemas/>
-        <Configuracion/>
-        <FRMregistro/>
-        <Tareas/>
-        <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<FRMregistro />} />
+          <Route path="/admin" element={<VistaAdmin />} />
+          <Route path="/usuario" element={<VistaUsuario />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
